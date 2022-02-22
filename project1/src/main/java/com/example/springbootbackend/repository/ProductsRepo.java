f9e1e3f337bcf6d3f9cc711720ecd3e77de0ea43
elka-t <97430516+elka-t@users.noreply.github.com>
2022-02-22 22:43:18 +0600

    Delete project1 directory

--- project1/src/main/java/com/example/springbootbackend/repository/ProductsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootbackend.repository;
-
-import com.example.springbootbackend.model.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProductsRepo extends JpaRepository<Products, Integer> {
-
-}
